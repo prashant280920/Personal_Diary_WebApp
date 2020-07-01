d076a13d8c4de3430c56496043fa645a029ad737
prashant280920 <jayantmehta8447442229@gmail.com>
2020-07-01 17:37:20 +0530

    Updates

---
+++ precache-manifest.d4094ad0e103432c7ca406e3b77eb879.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515f901f31c6d3f38354c69e46e77a11",
+    "url": "/Personal_Diary_WebApp/index.html"
+  },
+  {
+    "revision": "fc912b69751f88d56e57",
+    "url": "/Personal_Diary_WebApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0cb474f253471baa3066",
+    "url": "/Personal_Diary_WebApp/static/css/main.6851b416.chunk.css"
+  },
+  {
+    "revision": "fc912b69751f88d56e57",
+    "url": "/Personal_Diary_WebApp/static/js/2.836eafce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal_Diary_WebApp/static/js/2.836eafce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb474f253471baa3066",
+    "url": "/Personal_Diary_WebApp/static/js/main.5836b0bf.chunk.js"
+  },
+  {
+    "revision": "7a5b9e29565a206410ee",
+    "url": "/Personal_Diary_WebApp/static/js/runtime-main.b1f0adbc.js"
+  },
+  {
+    "revision": "c2c0420a3ba5ebc2f31a996b0b4f886e",
+    "url": "/Personal_Diary_WebApp/static/media/Diarywallper.c2c0420a.png"
+  }
+]);
