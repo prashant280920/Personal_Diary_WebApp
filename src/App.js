47be8d613219d54e1d95f61069d5aff5f6d027fe
prashant280920 <jayantmehta8447442229@gmail.com>
2020-07-06 20:20:47 +0530

    modified style

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 import Login from "./components/Login/Login.js";
 import Diary from "./components/Diary/diary.js";
 function App() {
-	 document.getElementById("root").style="width:100%";
+	 document.getElementById("root").style="width:100%;height:100%;";
   return (
 
   	// <Login />
-  	<div className="App ">
+  	<div className="full ">
+        <div className="center diaryname">
   	  		<h1>Diary Name</h1>
-  	  	<div className="App w-100 ">	
+        </div>
+  	  	<div className="App  w-100 ">	
   	      <Diary />
-  	      
   	     </div>
-  	  	</div>
+  	</div>
   );
 }
 
