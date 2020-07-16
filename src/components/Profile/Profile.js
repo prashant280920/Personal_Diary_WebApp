0197ce460f1b6fc0bf23e5262ed7197f9df4b4e8
prashant280920 <jayantmehta8447442229@gmail.com>
2020-07-16 21:09:43 +0530

    added mood option

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Calender from "./.././Calender/Calender.js";
+import Mood from "./Mood.js";
 
 const Profile = ({ Month, Year }) => {
 	console.log("Pro",Month,Year)
@@ -7,6 +8,8 @@ const Profile = ({ Month, Year }) => {
 		<div className=" pa2 profiles" style={{height:"100%",width:"100%",margin:"0px"}}>
 			<h1 className="p-cal"><u>Calender</u></h1>
 			<Calender Month={Month} Year={Year}/>
+			<h1 className="p-feel">How You feel Today?</h1>
+			<Mood />
 		</div>
 	);	
 	
