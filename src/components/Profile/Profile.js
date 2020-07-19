b5f3df3bd1e8c32b816dfcb205d3d898fe81d706
prashant280920 <jayantmehta8447442229@gmail.com>
2020-07-19 21:19:18 +0530

    added setting option

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calender from "./.././Calender/Calender.js";
 import Mood from "./Mood.js";
+import Emoji from ".././Emoji/Emoji.js";
 
-const Profile = ({ Month, Year }) => {
+const Profile = ({ Month, Year, rightClmChange }) => {
 	console.log("Pro",Month,Year)
 		return (
 		<div className=" pa2 profiles" style={{height:"100%",width:"100%",margin:"0px"}}>
+			 <Emoji onClick={rightClmChange} className="pointer settingIcon" symbol="⚙️" label="setting" />
 			<h1 className="p-cal"><u>Calender</u></h1>
 			<Calender Month={Month} Year={Year}/>
 			<h1 className="p-feel">How You feel Today?</h1>
