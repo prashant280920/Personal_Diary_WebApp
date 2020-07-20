f8daa56aea0b67209ca08a825f1da38daa2b2783
prashant280920 <jayantmehta8447442229@gmail.com>
2020-07-20 20:27:31 +0530

    modified file

--- src/components/Emoji/Emoji.js
+++ src/components/Emoji/Emoji.js
@@ -7,6 +7,9 @@ const Emoji = props => (
         aria-label={props.label ? props.label : ""}
         aria-hidden={props.label ? "false" : "true"}
         onClick={props.onClick ? props.onClick : function(){}}
+        onMouseOver={props.onMouseOver ? props.onMouseOver : function(){}}
+        onMouseOut={props.onMouseOut ? props.onMouseOut : function(){}}
+        title={props.title ? props.title : ""}
     >
         {props.symbol}
     </span>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,11 +3,12 @@ import Calender from "./.././Calender/Calender.js";
 import Mood from "./Mood.js";
 import Emoji from ".././Emoji/Emoji.js";
 
-const Profile = ({ Month, Year, rightClmChange }) => {
+const Profile = ({ Month, Year, rightClmChange , aboutChange, mouseOverImage, mouseOutImage}) => {
 	console.log("Pro",Month,Year)
 		return (
 		<div className=" pa2 profiles" style={{height:"100%",width:"100%",margin:"0px"}}>
-			 <Emoji onClick={rightClmChange} className="pointer settingIcon" symbol="⚙️" label="setting" />
+			<Emoji onClick={rightClmChange} className="pointer settingIcon dib" symbol="⚙️" label="setting" title="Setting"/>
+			 <Emoji onMouseOver={mouseOverImage} onMouseOut={mouseOutImage} onClick={aboutChange} className="imgChange grow pointer dib aboutIcon" symbol="📘" label="about" title="About Developers" />
 			<h1 className="p-cal"><u>Calender</u></h1>
 			<Calender Month={Month} Year={Year}/>
 			<h1 className="p-feel">How You feel Today?</h1>
